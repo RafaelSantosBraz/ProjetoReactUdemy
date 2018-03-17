438b82851274ba8d0bdd90f184ba0d008410f64a
RafaelSantosBraz <rafinha.santos.braz@gmail.com>
2018-03-17 01:01:26 -0300

    Construção do componente Titulo funcional

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,1 +1,14 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
 
+import Titulo from './titulo/titulo';
+
+let App = (
+    // sempre utilizar uma única estrutura
+    <div className="container">
+        <Titulo />
+    </div>
+);
+
+// componente para rendenizar o component 
+ReactDOM.render(App, document.getElementById("app"));
---
+++ src/js/titulo/titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Titulo extends React.Component {
+    // função de renderização de retorna o "html" desejado
+    render() {
+        return <h1>Curso de React com Gulp</h1>;
+    }
+}
+
+//exportação do componente
+export default Titulo;
