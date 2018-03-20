78ca0d6f2679f8b5dd19ea6a83bdffb88523cddc
RafaelSantosBraz <rafinha.santos.braz@gmail.com>
2018-03-19 23:26:05 -0300

    Finalização do projeto

--- src/js/cartao/lista-cartao.jsx
+++ src/js/cartao/lista-cartao.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// importação da biblioteca externa Axios
+import axios from 'axios';
 
 import Cartao from './cartao';
 import Busca from '../busca/busca'
@@ -43,33 +45,19 @@ class ListaCartao extends React.Component {
         // capturar o valor digitado no campo de busca
         this.setState({ busca: evento.target.value });
         // se o valor pesquisado for nulo, a lista toda é exibida
-        if (evento.target.value == ""){
-            this.setState({dados: this.state.servidor});
+        if (evento.target.value == "") {
+            this.setState({ dados: this.state.servidor });
         }
     }
     // executa quando o componente é criado
     componentDidMount() {
-        this.setState({
-            dados:
-                [
-                    { titulo: 'Título 1', descricao: 'Descrição 1', detalhe: 'Detalhe 1', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 2', descricao: 'Descrição 2', detalhe: 'Detalhe 2', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 3', descricao: 'Descrição 3', detalhe: 'Detalhe 3', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 4', descricao: 'Descrição 4', detalhe: 'Detalhe 4', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 5', descricao: 'Descrição 5', detalhe: 'Detalhe 5', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 6', descricao: 'Descrição 6', detalhe: 'Detalhe 6', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 7', descricao: 'Descrição 7', detalhe: 'Detalhe 7', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' }
-                ],
-            servidor:
-                [
-                    { titulo: 'Título 1', descricao: 'Descrição 1', detalhe: 'Detalhe 1', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 2', descricao: 'Descrição 2', detalhe: 'Detalhe 2', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 3', descricao: 'Descrição 3', detalhe: 'Detalhe 3', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 4', descricao: 'Descrição 4', detalhe: 'Detalhe 4', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 5', descricao: 'Descrição 5', detalhe: 'Detalhe 5', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 6', descricao: 'Descrição 6', detalhe: 'Detalhe 6', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' },
-                    { titulo: 'Título 7', descricao: 'Descrição 7', detalhe: 'Detalhe 7', imagem: 'http://materializecss.com/images/office.jpg', link: '#teste' }
-                ]
+        let self = this;
+        // faz a requisição ao servidor especificado, utilizando-se o Axios
+        axios.get('http://localhost:8000/servidor.php?dados=1').then(function (response) {
+            self.setState({
+                dados: response.data,
+                servidor: response.data
+            });
         });
     }
     // função de renderização de retorna o "html" desejado
--- src/js/navbar/navbar.jsx
+++ src/js/navbar/navbar.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+// importação da biblioteca externa Axios
+import axios from 'axios';
 
 // importação no mesmo diretório
 import NavMenu from './nav-menu'
 class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { menu: [] };
+    }
+    // executa quando o componente é criado
+    componentDidMount() {
+        let self = this;
+        // faz a requisição ao servidor especificado, utilizando-se o Axios
+        axios.get('http://localhost:8000/servidor.php?menu=1').then(function (response) {
+            self.setState({
+                menu: response.data,
+            });
+        });
+    }
     // função de renderização de retorna o "html" desejado
     render() {
         // menu de links para o navbar
-        let menu = [
-            // cada um dos objetos do array
-            { titulo: "Home", link: "#home" },
-            { titulo: "Sobre", link: "#sobre" },
-            { titulo: "Contato", link: "#contato" }
-        ];
+        let menu = this.state.menu;
         // classe atribuída dinamicamente
         let corNav = "nav-wrapper " + this.props.cor;
         return (
@@ -22,7 +33,7 @@ class Navbar extends React.Component {
                     <div className="container">
                         {/* Utilização do parâmetro recebido */}
                         <a href="#" className="brand-logo">{this.props.titulo}</a>
-                        <NavMenu menu={menu}/>                    
+                        <NavMenu menu={menu} />
                     </div>
                 </div>
             </nav>
